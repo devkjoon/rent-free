ca0687d0363579c6eacff707865a52783848943e
joonkidk <joonkidk@gmail.com>
2023-09-25 21:38:12 -0400

    mongodb connection successful

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+import { DATABASE } from "./config.js";
 
 const app = express();
+
+//db
+mongoose.set("strictQuery", false);
+mongoose.connect(DATABASE).then(() => console.log("db_connected"))
+.catch((err) => console.log(err));
 
 // middlewares
 app.use(express.json());
