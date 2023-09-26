9c0c5be977e1e769a5bde7e6e451b610f5f7ee3e
devkjoon <devkjoon@gmail.com>
2023-09-26 16:29:33 -0400

    minor updates'

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,8 +5,19 @@ export const welcome = (req, res) => {
 };
 
 export const preRegister = async (req, res) => {
+  // create jwt with email and password then email as clickable link
+  // only when user clicks on on that email link, registration will be complete
   try {
-    //
+    console.log(req.body);
+
+    const emailSent = true;
+
+    if(emailSent) {
+      return res.json({ ok: true });
+    } else {
+      return res.json({ ok: false });
+    }
+
   } catch (err) {
     console.log(err);
     return res.json({ error: "Something went wrong. Please try again." });
