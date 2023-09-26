34148f4aff076685315cdf29005cb5b346259df3
devkjoon <devkjoon@gmail.com>
2023-09-26 14:52:21 -0400

    created overall schema

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,39 +1,45 @@
-import {model, Schema, ObjectId} from 'mongoose';
+import { model, Schema, ObjectId } from "mongoose";
 
-const schema = new Schema({
+const schema = new Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        lowercase: true,
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
     name: {
-        type: String,
-        trime: true,
-        default: "",
+      type: String,
+      trime: true,
+      default: "",
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        lowercase: true,
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: true,
-        maxLength: 256,
+      type: String,
+      required: true,
+      maxLength: 256,
     },
-    address: {type: String, default:""},
-    company: {type: String, default:""},
-    phone: {type: String, default:""},
+    address: { type: String, default: "" },
+    company: { type: String, default: "" },
+    phone: { type: String, default: "" },
     photo: {},
     role: {
-        type: [String],
-        default: ['Buyer'],
-        enum: ['Buyer', 'Seller', 'Admin'],
+      type: [String],
+      default: ["Buyer"],
+      enum: ["Buyer", "Seller", "Admin"],
     },
-});
+    enquiredProperties: [{ type: ObjectId, ref: "Ad" }],
+    wishlist: [{ type: ObjectId, ref: "Ad" }],
+    resetCode: "",
+  },
+  { timestamps: true }
+);
 
-export default model('User', schema);
+export default model("User", schema);
