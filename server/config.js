12dff08571d5237b4812a60f173eb959a6539b0b
joonkidk <joonkidk@gmail.com>
2023-09-26 22:18:42 -0400

    added aws access key and secret key

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export const DATABASE = "mongodb://127.0.0.1:27017/rent-free";
+
+export const AWS_ACCESS_KEY_ID = "AKIAY67G4KKDF322ZDFA";
+export const AWS_SECRET_ACCESS_KEY = "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";
